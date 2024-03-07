7e352bdd9931f0d955860acbf49bc20620618371
AlfaNaufal <alfanaufal79@gmail.com>
2024-03-07 17:02:21 +0700

    Update ui

--- lib/view/homepage.dart
+++ lib/view/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ukom_app/constant/color.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,16 +9,87 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool? visible = true;
+  final _formkey = GlobalKey<FormState>();
+  var usernameController = TextEditingController();
+  var passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: bgcolor,
       appBar: AppBar(
-        backgroundColor: Colors.amberAccent,
-        title: Text('Home Page'),
+        backgroundColor: tdgreen,
+        title: Text(
+          'Home Page',
+          style: TextStyle(color: Colors.white),
+        ),
       ),
       body: Container(
-        alignment: Alignment.center,
-        child: Text('data'),
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Form(
+          key: _formkey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            // crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Login",
+                style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.9,
+                // height: MediaQuery.of(context).size.height,
+                alignment: Alignment.center,
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      hintText: "Nama",
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.black),
+                          borderRadius: BorderRadius.circular(100))),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.9,
+                // height: MediaQuery.of(context).size.height,
+                alignment: Alignment.center,
+                child: TextFormField(
+                  controller: passwordController,
+                  obscureText: visible!,
+                  obscuringCharacter: '●',
+                  style: TextStyle(),
+                  decoration: InputDecoration(
+                    hintText: "Password",
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black),
+                        borderRadius: BorderRadius.circular(100)),
+                    suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            visible = !visible!;
+                          });
+                        },
+                        icon: visible == false
+                            ? Icon(Icons.visibility)
+                            : Icon(Icons.visibility_off)),
+                  ),
+                ),
+              ),
+              Container(
+                  width: MediaQuery.of(context).size.width ,
+                  height: 50,
+                  child:
+                      ElevatedButton(onPressed: () {}, child: Text('Login'))),
+            ],
+          ),
+        ),
       ),
     );
   }
